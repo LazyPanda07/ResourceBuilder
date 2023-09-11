605cb83f1c97455b5d355b1597a14dee16c8c1dc
LazyPanda07 <semengricenko@gmail.com>
2023-09-11 16:59:47 +0500

    Test

--- tests/generate_and_run/main.cpp
+++ tests/generate_and_run/main.cpp
@@ -38,6 +38,8 @@ int main(int argc, char** argv)
 
         std::cout << "Executed command: " << command << std::endl;
 
+        system("where cl.exe");
+
         if (system(command.data()))
         {
             return 1;
